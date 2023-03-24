99715fac25a8a7b823b34b70bbe279252c1626e6
micro7-PhongTrinh <micro7.quocphong@gmail.com>
2023-03-24 10:01:26 +0700

    change db connection string

--- connection.js
+++ connection.js
@@ -1,10 +1,14 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '123456',
-    database: 'one_read',
-    port: 5432,
+    // user: 'postgres',
+    // password: '123456',
+    // database: 'one_read',
+    // port: 5432,
+    connectionString: 'postgres://reading_db_b0mg_user:tqWPCaqf2YSi40HOWCwgD6XeGB9nbDlS@dpg-cgegbn9mbg568r1e4q1g-a.oregon-postgres.render.com/reading_db_b0mg',
+    ssl: {
+		rejectUnauthorized: false
+	}
 })
 
 pool.on('error', (err) => {
